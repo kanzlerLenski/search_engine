255cc456cd72927c87ec7722f0e125022434aa8c
kanzlerlenski <44209042+kanzlerlenski@users.noreply.github.com>
2021-07-27 00:33:07 +0300

    Update test_stemmer.py

--- tests/test_stemmer.py
+++ tests/test_stemmer.py
@@ -5,30 +5,33 @@ class Tests(unittest.TestCase):
 
     def setUp(self):
         self.s4 = 'столовая'
+        self.s3 = 'ну'
         self.stmr = stemmer.Stemmer('flexions.txt')
 
     # Check if all TypeErrors are raised.
     def test_string(self):
         s = 5
         s2 = ''
-        s3 = 'ну'
         with self.assertRaises(TypeError):
             list(self.stmr.pseudostem_generator(s))
         with self.assertRaises(TypeError):
             list(self.stmr.pseudostem_generator(s2))
-        with self.assertRaises(TypeError):
-            list(self.stmr.pseudostem_generator(s3))
+        #with self.assertRaises(TypeError):
+            #list(self.stmr.pseudostem_generator(self.s3))
         with self.assertRaises(TypeError):
             list(self.stmr.pseudostem_generator_with_check(s))
         with self.assertRaises(TypeError):
             list(self.stmr.pseudostem_generator_with_check(s2))
-        with self.assertRaises(TypeError):
-            list(self.stmr.pseudostem_generator_with_check(s3))
+        #with self.assertRaises(TypeError):
+            #list(self.stmr.pseudostem_generator_with_check(self.s3))
 
     # Check if presudostem_generator yields all pseudostems. 
     def test_pseudostem_generator(self):
         res = self.stmr.pseudostem_generator(self.s4)
         self.assertEqual(list(res), ['столовая', 'столова', 'столов', 'столо'])
+
+        res2 = self.stmr.pseudostem_generator(self.s3)
+        self.assertEqual(list(res2), ['ну'])
 
     # Check if presudostem_generator yields all pseudostems. 
     def test_pseudostem_generator_with_check(self):
